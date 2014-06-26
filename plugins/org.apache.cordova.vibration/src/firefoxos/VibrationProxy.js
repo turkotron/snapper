564eeb640da612327f831ec926780bad4c3b9d34
Sebastien Renault <sre@zenexity.com>
2014-06-26 22:43:04 +0200

    Add vibration plugin & clock page

--- plugins/org.apache.cordova.vibration/src/firefoxos/VibrationProxy.js
+++ plugins/org.apache.cordova.vibration/src/firefoxos/VibrationProxy.js
@@ -1,4 +1,5 @@
 /*
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -7,7 +8,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -15,19 +16,20 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- */
-* {
-    -webkit-tap-highlight-color: rgba(0,0,0,0); /* make transparent link selection, adjust last value opacity 0 to 1.0 */
-}
+ *
+*/
 
-body {
-    -webkit-touch-callout: none;                /* prevent callout to copy image, etc when tap to hold */
-    -webkit-text-size-adjust: none;             /* prevent webkit from resizing text to fit */
-    -webkit-user-select: none;                  /* prevent copy paste, to allow, change 'none' to 'text' */
-    font-family:'HelveticaNeue-Light', 'HelveticaNeue', Helvetica, Arial, sans-serif;
-    font-size:12px;
-    height:100%;
-    margin:0px;
-    padding:0px;
-    width:100%;
-}
+var cordova = require('cordova');
+
+module.exports = {
+
+    vibrate: function(success, fail, milliseconds) {
+        if (navigator.notification.vibrate) {
+            navigator.vibrate(milliseconds);
+        } else {
+            console.log ("cordova/plugin/firefoxos/vibration, vibrate API does not exist");
+        }
+    }
+};
+
+require("cordova/firefoxos/commandProxy").add("Vibration", module.exports);
